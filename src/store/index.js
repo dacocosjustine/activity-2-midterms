3433853b9868ce7237242847000cbf26f268ed8e
Shashimii <besasamreginald36@gmail.com>
2024-04-19 16:09:01 +0800

    fixes the mutation for adding to the array with id parameter for incrementation every item and add a action for adding products

--- src/store/index.js
+++ src/store/index.js
@@ -2,27 +2,33 @@ import { createStore } from 'vuex'
 
 export default createStore({
     state: {
-        products: [
-
-            {id: 1, name: 'Smartphone', desc: 'High-end smartphone with advanced features.', price: 12399},
-            {id: 2, name: 'Laptop', desc: 'Powerful laptop for work and gaming.', price: 39999},
-            {id: 3, name: 'Wireless Headphones', desc: 'Premium wireless headphones with noise-cancellation.', price: 990},
-            {id: 4, name: 'Smartwatch', desc: 'Feature-packed smartwatch with fitness tracking.', price: 7099},
-            {id: 5, name: 'Smart TV', desc: 'High-definition smart TV with built-in streaming apps.', price: 54990},
-        ]
+        products: [],
+        productId: 0
     },
+
     getters: {
         productsList: state => state.products
     },
+
     mutations: {
         addProduct(state, product) {
-            state.products.push(product)
+            state.products.push({
+              id: state.productId,
+              ...product
+            });
+            state.productId++;
         },
+
         deleteProduct(state, id) {
             state.products.splice(id, 1);
         },
         updateProduct(state, product) {
             state.products[product.id] = product;
+        }
+    },
+    actions: {
+        addProduct({ commit }, product) {
+          commit('addProduct', product);
         }
     }
 })
